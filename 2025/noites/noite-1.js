e0a647cadc715ab95f789de8778db24b478abfde
uctq <umacontatestequalquer127@gmail.com>
2026-07-22 01:10:06 -0300

    Version X (26.2.4)

--- 2025/noites/noite-1.js
+++ 2025/noites/noite-1.js
@@ -7,21 +7,21 @@ NOITES[1] = {
   subtitulo: 'Turmas que se apresentaram na primeira noite do festival.',
   pecas: [
     {
-      titulo: 'É Vero, Verissimos!',
+      titulo: 'Nota de Repúdio',
       turma: '01',
       sinopse: 'Em uma escola rígida e opressiva na Caxias do Sul de 1995, o silêncio é norma e a obediência é lei. Tudo muda quando Elisa encontra uma gravata colorida, esquecida no chão. Um símbolo aparentemente simples, mas que desperta curiosidade, coragem e, principalmente, resistência. À medida que a tensão se intensifica, a arte, os sonhos e a liberdade, há muito tempo enterrados, voltam a emergir. Nota de Repúdio é uma peça sobre o poder da arte, o peso do silêncio e a força de um grupo que ousa dizer "basta".',
       youtube: 'n59xdzLBBA8&t',        // id do vídeo (deixe '' se ainda não tiver)
       youtubeInicio: 2317            // segundo em que a peça começa no vídeo
     },
     {
-      titulo: 'Calados pela Morte',
+      titulo: ' A estrela das manhãs',
       turma: 'G3',
       sinopse: 'Adriano é um ator fracassado que enfrenta uma crise em seu casamento vivendo às custas de sua mulher. Entretanto, tudo muda quando ele assume o papel de Truco, um palhaço caótico que se torna a sensação das manhãs na TV brasileira. Em meio aos risos, gritos e contratos, Adriano finalmente sente que encontrou seu lugar. Só que os holofotes brilham mais forte do que esperava e, seduzido pelos bastidores, entre bebidas e escolhas duvidosas, ele se vê tropeçando no mesmo palco que o consagrou. Uma história de ascensão e queda, onde o riso esconde o vazio de um homem que só queria ser visto.',
       youtube: 'n59xdzLBBA8&t',
       youtubeInicio: 4585
     },
     {
-      titulo: 'O Clube dos Anjos - O que não engorda, mata',
+      titulo: 'O que ficou da gente',
       turma: 'B3',
       sinopse: 'Anos após se formarem no CETEC, um grupo de antigos colegas do Ensino Médio se reencontra no nostálgico "Bar da Beta", onde tantas histórias começaram. Entre risadas e atualizações de vida, Lúcia traz uma caixa repleta de fotografias da época escolar. As imagens, porém, despertam mais que lembranças: revelam segredos, alimentam ressentimentos e reacendem sentimentos adormecidos. O que seria apenas uma noite de reencontro se transforma numa intensa viagem pelos caminhos que cada um seguiu. Em meio a romances interrompidos, mágoas e escolhas não planejadas, o grupo revive os altos e baixos da juventude com humor, drama e afeto. Uma história humanizada sobre os imprevistos da vida e as perguntas que só o tempo pode tentar responder..',
       youtube: 'n59xdzLBBA8&t',
