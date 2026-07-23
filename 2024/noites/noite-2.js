89baaaac12da2b6c544f53e9bbb9c8ccbab33ab1
uctq <umacontatestequalquer127@gmail.com>
2026-07-22 23:46:35 -0300

    Version X (26.4.2)
    2024 Bug Fix

--- 2024/noites/noite-2.js
+++
@@ -1,9 +0,0 @@
-/* ===== NOITE 2 — CETEC FESTIVAL 2025 ===== */
-
-NOITES[2] = {
-  data: '4-07-09T19:00:00-03:00',
-  subtitulo: 'Turmas que se apresentaram na segunda noite do festival.',
-  pecas: [
-    
-  ]
-};
